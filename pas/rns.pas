a21f3bfcd50cb27f3702fc91ddd3ff61a90992d1
Rico Pajarola <rp@servium.ch>
2025-08-30 21:48:52 +0200

    remove unused Use statements from rns.pas and delete now unused userexit unit

--- pas/rns.pas
+++ pas/rns.pas
@@ -1,20 +1,10 @@
 ﻿{$I RNS.H}
 Program Rns(Input, Output);
 
-Uses Graph,
-    crt,
-    Dos,
-    initsc,
-    UserInt,
-    EditUnit,
-    menutyp,
-    helpunit,
-    userexit;
+Uses InitSc,
+    UserInt;
 
 Begin
     IniIniColors;
     UseTopMenu;
-    TextBackground (black);
-    TextColor (white);
-    ClrScr;
 End.
